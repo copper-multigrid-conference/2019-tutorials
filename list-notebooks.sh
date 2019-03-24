31296409c3662a24eae5ee78dbabba2bc5627f42
Luke Olson <luke.olson@gmail.com>
2019-03-24 09:58:17 -0600

    add to list

--- list-notebooks.sh
+++ list-notebooks.sh
@@ -1,4 +1,7 @@
 #!/usr/local/bin/zsh
 
+for f in multigrid/*.ipynb;
+    echo '- ['$f:t']''( https://colab.research.google.com/github/copper-multigrid-conference/2019-tutorials/blob/master/'$f')'
+
 for f in algebraic-multigrid/*.ipynb;
     echo '- ['$f:t']''( https://colab.research.google.com/github/copper-multigrid-conference/2019-tutorials/blob/master/'$f')'
